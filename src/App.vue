59915ee40ac53de62cacc31fab04885621fa44dd
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-03-25 14:22:25 +0100

    aggiunto bonus jumbotron

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hero msg="This is a jumbotron on Vue.js"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Hero
   }
 }
 </script>
